d8d6f5d08ad03b8bf1377609911832cdab95e206
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-08-24 21:21:07 +0530

    activity 1 some edits

--- Activity_1/login.js
+++ Activity_1/login.js
@@ -6,8 +6,5 @@ var log_in_btn = document.getElementById("log_in_btn").addEventListener("click",
 var sign_up_btn = document.getElementById("sign_up_btn").addEventListener("click", ()=>{
     var username = document.getElementById("username");
     var password = document.getElementById("pass");
-
-    
-
-})
+});
 
--- Activity_1/server.js
+++ Activity_1/server.js
@@ -1,1 +1,13 @@
-var app = require('')
+const express = require('express');
+const passport = require('passport');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/",(req, res)=>{
+    res.render("index");
+});
+
+app.listen(3000);
